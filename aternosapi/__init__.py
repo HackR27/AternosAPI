3fb84f265b15f51118189c2ec7f07ee75d6c9d47
HackR27 <HackR27@users.noreply.github.com>
2020-11-09 23:56:28 +0530

    Update __init__.py
    Changed the parameters to avoid ambiguity

--- aternosapi/__init__.py
+++ aternosapi/__init__.py
@@ -3,13 +3,13 @@ import json
 from user_agent import generate_user_agent
 
 class AternosAPI(StatefulBrowser):
-    def __init__(self, headers):
+    def __init__(self, cookies):
         self.headers = {}
         self.reqheaders = {}
         self.cookies = {}
         self.user_agent = generate_user_agent()
         self.headers['User-Agent'] = self.user_agent
-        self.headers['Cookie'] = headers
+        self.headers['Cookie'] = cookies
         super().__init__(soup_config={'features':'lxml'},raise_on_404=True,user_agent=self.user_agent) 
         # self.SEC = SEC
         self.JavaSoftwares = ['Vanilla', 'Spigot', 'Forge', 'Magma', 'Snapshot', 'Bukkit', 'Paper', 'Modpacks', 'Glowstone']
@@ -101,5 +101,5 @@ class AternosAPI(StatefulBrowser):
             return data.text.strip('\n')
 
 if __name__ == "__main__":
-    api = AternosAPI(headers='__cfduid=d3fa0d88095829528bb6f323b7825b84a1604714248; ATERNOS_LANGUAGE=en; PHPSESSID=3bb7k35r4evv526oonriot8kkj; ATERNOS_STYLE=dark; _ga=GA1.2.1611040091.1604715330; _gid=GA1.2.1685488237.1604715330; __gads=ID=c4b56dfe7041e604:T=1604715332:S=ALNI_MZ7gSk_v__suK1mFh624f4lVF-8wA; cnx_userId=e5b6a491d804497c9d085a64cd7a007e; fileDownload=true; SKpbjs-id5id=%7B%22ID5ID%22%3A%22ID5-ZHMOHk6CB7VbgfhNpd2eqFFHiixfg79Nw6CZgnoVBQ%22%2C%22ID5ID_CREATED_AT%22%3A%222020-11-07T02%3A15%3A30Z%22%2C%22ID5_CONSENT%22%3Atrue%2C%22CASCADE_NEEDED%22%3Atrue%2C%22ID5ID_LOOKUP%22%3Atrue%2C%223PIDS%22%3A%5B%5D%7D; ATERNOS_SESSION=GAzZ83qTl5xNInA34b08JMFpv7rdaYPnxwHizvwajiMIGaVLRUc3I9wOTdTTaSsRwCeHuHUEDeDQYdlAKmI6PIgL7rwHY3vnfrsM; ATERNOS_SERVER=1e3IweOlsYTdbTms; cto_bidid=yCbdSl9ob3c4YjF1anB5d2N5c1glMkJRa05NMXVnc2lrTW5jZW5jWVQ5NGs1TE5GYTQ4JTJCRTAyT1FJWHdidmtpTHVqUGtwOXQ5Mm9HSW55a2FkcSUyQmZYVzJLaHpVQSUzRCUzRA; cto_bundle=otMBGV9QJTJCUlUySlRuV3U4YXdIdjZXYzEwRjNYSnpxdDJUaGdEbUtzbUliWFpxelA5Wmx0aUdlQTZvbTBxR1glMkI4d0klMkZpNUhiU0hQZlJGZFlvWmRGJTJGMWNJUVQ2cXhzeEt4SHRGc2p4NVc4WTNKbDlQcnM1SDFUWU1oZkZ6RTBSRlc0ZnglMkI; SKpbjs-unifiedid=%7B%22TDID%22%3A%22d1a2a172-8786-4dcb-8ddd-abef54981613%22%2C%22TDID_LOOKUP%22%3A%22TRUE%22%2C%22TDID_CREATED_AT%22%3A%222020-10-09T03%3A32%3A14%22%7D; SKpbjs-unifiedid_last=Mon%2C%2009%20Nov%202020%2003%3A32%3A15%20GMT; SKpbjs-id5id_last=Mon%2C%2009%20Nov%202020%2003%3A32%3A15%20GMT; id5id.1st_364_nb=5; GED_PLAYLIST_ACTIVITY=W3sidSI6IjhYbWoiLCJ0c2wiOjE2MDQ5MTEwNzAsIm52IjoxLCJ1cHQiOjE2MDQ5MDk1NjUsImx0IjoxNjA0OTExMDY5fV0')
+    api = AternosAPI(cookies='__cfduid=d3fa0d88095829528bb6f323b7825b84a1604714248; ATERNOS_LANGUAGE=en; PHPSESSID=3bb7k35r4evv526oonriot8kkj; ATERNOS_STYLE=dark; _ga=GA1.2.1611040091.1604715330; _gid=GA1.2.1685488237.1604715330; __gads=ID=c4b56dfe7041e604:T=1604715332:S=ALNI_MZ7gSk_v__suK1mFh624f4lVF-8wA; cnx_userId=e5b6a491d804497c9d085a64cd7a007e; fileDownload=true; SKpbjs-id5id=%7B%22ID5ID%22%3A%22ID5-ZHMOHk6CB7VbgfhNpd2eqFFHiixfg79Nw6CZgnoVBQ%22%2C%22ID5ID_CREATED_AT%22%3A%222020-11-07T02%3A15%3A30Z%22%2C%22ID5_CONSENT%22%3Atrue%2C%22CASCADE_NEEDED%22%3Atrue%2C%22ID5ID_LOOKUP%22%3Atrue%2C%223PIDS%22%3A%5B%5D%7D; ATERNOS_SESSION=GAzZ83qTl5xNInA34b08JMFpv7rdaYPnxwHizvwajiMIGaVLRUc3I9wOTdTTaSsRwCeHuHUEDeDQYdlAKmI6PIgL7rwHY3vnfrsM; ATERNOS_SERVER=1e3IweOlsYTdbTms; cto_bidid=yCbdSl9ob3c4YjF1anB5d2N5c1glMkJRa05NMXVnc2lrTW5jZW5jWVQ5NGs1TE5GYTQ4JTJCRTAyT1FJWHdidmtpTHVqUGtwOXQ5Mm9HSW55a2FkcSUyQmZYVzJLaHpVQSUzRCUzRA; cto_bundle=otMBGV9QJTJCUlUySlRuV3U4YXdIdjZXYzEwRjNYSnpxdDJUaGdEbUtzbUliWFpxelA5Wmx0aUdlQTZvbTBxR1glMkI4d0klMkZpNUhiU0hQZlJGZFlvWmRGJTJGMWNJUVQ2cXhzeEt4SHRGc2p4NVc4WTNKbDlQcnM1SDFUWU1oZkZ6RTBSRlc0ZnglMkI; SKpbjs-unifiedid=%7B%22TDID%22%3A%22d1a2a172-8786-4dcb-8ddd-abef54981613%22%2C%22TDID_LOOKUP%22%3A%22TRUE%22%2C%22TDID_CREATED_AT%22%3A%222020-10-09T03%3A32%3A14%22%7D; SKpbjs-unifiedid_last=Mon%2C%2009%20Nov%202020%2003%3A32%3A15%20GMT; SKpbjs-id5id_last=Mon%2C%2009%20Nov%202020%2003%3A32%3A15%20GMT; id5id.1st_364_nb=5; GED_PLAYLIST_ACTIVITY=W3sidSI6IjhYbWoiLCJ0c2wiOjE2MDQ5MTEwNzAsIm52IjoxLCJ1cHQiOjE2MDQ5MDk1NjUsImx0IjoxNjA0OTExMDY5fV0')
     api.StopServer()
