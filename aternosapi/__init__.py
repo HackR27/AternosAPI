bccaa12833946761b54f90d5b46fa1bf5b2aef9e
HackR27 <gahin.kgp@gmail.com>
2020-11-10 09:28:56 +0530

    Added login method

--- aternosapi/__init__.py
+++ aternosapi/__init__.py
@@ -1,5 +1,6 @@
 from mechanicalsoup import StatefulBrowser
 import json
+import requests,cloudscraper
 from user_agent import generate_user_agent
 
 class AternosAPI(StatefulBrowser):
@@ -80,11 +81,17 @@ class AternosAPI(StatefulBrowser):
                 i = 1
                 return "Server Started"
 
+    def login(self):
+        self.setSec()
+        data = self.post('https://aternos.org/panel/ajax/account/login.php',params={'user':'HackR27','password':"a881d0db56264a6f3e7650c243971f10",'SEC':self.sec},self.reqheaders)
+        print(data.status_code)
+
     def setSec(self):
         key = self.keygen()
         value = self.keygen()
-        self.reqheaders['User-Agent'] = self.user_agent
-        self.reqheaders['Cookie'] = f'ATERNOS_SEC_{key}={value}; '+self.headers['Cookie']
+        cloud = self.getCookie()
+        self.reqheaders['User-Agent'] = cloud[1]
+        self.reqheaders['Cookie'] = f'ATERNOS_SEC_{key}={value}; '+cloud[0].split(' ')[0]
         self.sec = key+':'+value
 
     def keygen(self):
@@ -100,6 +107,10 @@ class AternosAPI(StatefulBrowser):
         if data.status_code != 503:
             return data.text.strip('\n')
 
+    def getCookie(self):
+        data = cloudscraper.get_cookie_string('https://aternos.org/:en')
+        return data
+
 if __name__ == "__main__":
     api = AternosAPI(cookies='__cfduid=d3fa0d88095829528bb6f323b7825b84a1604714248; ATERNOS_LANGUAGE=en; PHPSESSID=3bb7k35r4evv526oonriot8kkj; ATERNOS_STYLE=dark; _ga=GA1.2.1611040091.1604715330; _gid=GA1.2.1685488237.1604715330; __gads=ID=c4b56dfe7041e604:T=1604715332:S=ALNI_MZ7gSk_v__suK1mFh624f4lVF-8wA; cnx_userId=e5b6a491d804497c9d085a64cd7a007e; fileDownload=true; SKpbjs-id5id=%7B%22ID5ID%22%3A%22ID5-ZHMOHk6CB7VbgfhNpd2eqFFHiixfg79Nw6CZgnoVBQ%22%2C%22ID5ID_CREATED_AT%22%3A%222020-11-07T02%3A15%3A30Z%22%2C%22ID5_CONSENT%22%3Atrue%2C%22CASCADE_NEEDED%22%3Atrue%2C%22ID5ID_LOOKUP%22%3Atrue%2C%223PIDS%22%3A%5B%5D%7D; ATERNOS_SESSION=GAzZ83qTl5xNInA34b08JMFpv7rdaYPnxwHizvwajiMIGaVLRUc3I9wOTdTTaSsRwCeHuHUEDeDQYdlAKmI6PIgL7rwHY3vnfrsM; ATERNOS_SERVER=1e3IweOlsYTdbTms; cto_bidid=yCbdSl9ob3c4YjF1anB5d2N5c1glMkJRa05NMXVnc2lrTW5jZW5jWVQ5NGs1TE5GYTQ4JTJCRTAyT1FJWHdidmtpTHVqUGtwOXQ5Mm9HSW55a2FkcSUyQmZYVzJLaHpVQSUzRCUzRA; cto_bundle=otMBGV9QJTJCUlUySlRuV3U4YXdIdjZXYzEwRjNYSnpxdDJUaGdEbUtzbUliWFpxelA5Wmx0aUdlQTZvbTBxR1glMkI4d0klMkZpNUhiU0hQZlJGZFlvWmRGJTJGMWNJUVQ2cXhzeEt4SHRGc2p4NVc4WTNKbDlQcnM1SDFUWU1oZkZ6RTBSRlc0ZnglMkI; SKpbjs-unifiedid=%7B%22TDID%22%3A%22d1a2a172-8786-4dcb-8ddd-abef54981613%22%2C%22TDID_LOOKUP%22%3A%22TRUE%22%2C%22TDID_CREATED_AT%22%3A%222020-10-09T03%3A32%3A14%22%7D; SKpbjs-unifiedid_last=Mon%2C%2009%20Nov%202020%2003%3A32%3A15%20GMT; SKpbjs-id5id_last=Mon%2C%2009%20Nov%202020%2003%3A32%3A15%20GMT; id5id.1st_364_nb=5; GED_PLAYLIST_ACTIVITY=W3sidSI6IjhYbWoiLCJ0c2wiOjE2MDQ5MTEwNzAsIm52IjoxLCJ1cHQiOjE2MDQ5MDk1NjUsImx0IjoxNjA0OTExMDY5fV0')
-    api.StopServer()
+    api.login()
